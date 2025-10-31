cef0929f84759bf1704d80ddd4cbcbc861a1b68d
acroxjvxx <twacrox@gmail.com>
2025-10-31 15:33:03 +0100

    Aggiornamento automatico

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, shell, dialog } = require('electron');
 const path = require('path');
 const fs = require('fs');
 const https = require('https');
-const { exec } = require('child_process');
+const unzipper = require('unzipper');
 
 let mainWindow;
 
@@ -14,7 +14,7 @@ function createWindow() {
       nodeIntegration: true,
       contextIsolation: false
     },
-    icon: path.join(__dirname, 'icon.png')
+    icon: path.join(__dirname, 'icon.ico')
   });
 
   mainWindow.setMenu(null);
@@ -29,7 +29,7 @@ function createWindow() {
   });
 }
 
-// Controlla se ci sono aggiornamenti
+// Controllo aggiornamenti
 function checkForUpdates() {
   const localVersionPath = path.join(__dirname, 'version.txt');
   const remoteVersionUrl = 'https://raw.githubusercontent.com/acroxjvxx/acrox-launcher/main/version.txt';
@@ -48,4 +48,64 @@ function checkForUpdates() {
           type: 'info',
           buttons: ['Aggiorna ora', 'Annulla'],
           title: 'Aggiornamento disponibile',
-          message: `È disponibile una nuova versione (${remoteVersion}).
+          message: `È disponibile una nuova versione (${remoteVersion}). Vuoi aggiornare ora?`
+        });
+
+        if (choice === 0) {
+          downloadUpdate(remoteVersion);
+        }
+      }
+    });
+  }).on('error', (err) => console.error('Errore controllo aggiornamento:', err));
+}
+
+// Scarica aggiornamento con barra progresso
+function downloadUpdate(remoteVersion) {
+  const zipUrl = 'https://github.com/acroxjvxx/acrox-launcher/archive/refs/heads/main.zip';
+  const tempPath = path.join(app.getPath('temp'), 'update.zip');
+  const file = fs.createWriteStream(tempPath);
+
+  let receivedBytes = 0;
+  let totalBytes = 0;
+
+  mainWindow.loadURL('data:text/html,<h1 style="font-family:sans-serif;text-align:center;">Scaricamento aggiornamento... 0%</h1>');
+
+  https.get(zipUrl, (res) => {
+    totalBytes = parseInt(res.headers['content-length'], 10);
+
+    res.on('data', chunk => {
+      receivedBytes += chunk.length;
+      let percent = Math.floor((receivedBytes / totalBytes) * 100);
+      mainWindow.loadURL(`data:text/html,<h1 style="font-family:sans-serif;text-align:center;">Scaricamento aggiornamento... ${percent}%</h1>`);
+    });
+
+    res.pipe(file);
+
+    file.on('finish', () => {
+      file.close();
+      // Estrarre ZIP (sovrascrive files locali)
+      fs.createReadStream(tempPath)
+        .pipe(unzipper.Extract({ path: __dirname }))
+        .on('close', () => {
+          fs.writeFileSync(path.join(__dirname, 'version.txt'), remoteVersion);
+          dialog.showMessageBoxSync({
+            type: 'info',
+            title: 'Aggiornamento completato',
+            message: 'Aggiornamento completato! Riavvia il launcher per applicarlo.'
+          });
+        });
+    });
+  }).on('error', (err) => {
+    console.error('Errore download aggiornamento:', err);
+    dialog.showErrorBox('Errore', 'Non è stato possibile scaricare l\'aggiornamento.');
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  checkForUpdates();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
